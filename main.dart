56c584f6667ff78715e8f2a416b644512efec2c4
hamzaAbid <sixtynine930@gmail.com>
2024-08-26 18:29:10 +0500

    flutterclass1

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){ 
+  var assignment ="INTRODUCTION";
+  var name ="HamzaAbid";
+  int age =18;
+  int myclass =12;
+  double percentage =48.7;
+  var previousresult ="pass";
+  print (assignment);
+  print (name);
+  print (age);
+  print (myclass);
+  print (percentage);
+  print (previousresult);
+}
